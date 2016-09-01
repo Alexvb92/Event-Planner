86157785f3411d534dd20daa2660dc14dea9eb4a
Houston Breedlove <wehaveaproblem16@guest-2-00315.dhcp.unc.edu>
2016-09-01 19:27:56 -0400

    initial

--- assets/javascript/planner.js
+++ assets/javascript/planner.js
@@ -2,6 +2,10 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
    $('#startButton').on('click', function () {
-   	
+   		destination = $('#destination').val().trim();
+   		startDate = $('#startDate').val();  //might need adjustment for moment.js
+   		endDate = $('#endDate').val(); //might need adjustment for moment.js
+
+   		
    })
 });
