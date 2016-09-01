732d3276aefbdbc5351ee8be47a9af2bc92ffcfb
Houston Breedlove <wehaveaproblem16@guest-2-00315.dhcp.unc.edu>
2016-09-01 19:34:01 -0400

    fade out

--- assets/javascript/planner.js
+++ assets/javascript/planner.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-// var config = {
-// 	apiKey: "AIzaSyBadNobcnUDFdZpD_h7AFyn-V5AhLV4M34",
-// 	authDomain: "gottogo-dbe65.firebaseapp.com",
-// 	databaseURL: "https://gottogo-dbe65.firebaseio.com",
-// 	storageBucket: "gottogo-dbe65.appspot.com",
-// };
+var config = {
+	apiKey: "AIzaSyBadNobcnUDFdZpD_h7AFyn-V5AhLV4M34",
+	authDomain: "gottogo-dbe65.firebaseapp.com",
+	databaseURL: "https://gottogo-dbe65.firebaseio.com",
+	storageBucket: "gottogo-dbe65.appspot.com",
+};
   
-// firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-// database = firebase.database();
+database = firebase.database();
 
 $('#startButton').on('click', function () {
 	destination = $('#destination').val().trim();
