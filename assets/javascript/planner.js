cf361b6c21b5e099a583ebdec1511351ab240f41
Houston Breedlove <wehaveaproblem16@guest-2-00315.dhcp.unc.edu>
2016-09-01 19:33:06 -0400

    start button click and fade out page

--- assets/javascript/planner.js
+++ assets/javascript/planner.js
@@ -1,11 +1,25 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-   $('#startButton').on('click', function () {
-   		destination = $('#destination').val().trim();
-   		startDate = $('#startDate').val();  //might need adjustment for moment.js
-   		endDate = $('#endDate').val(); //might need adjustment for moment.js
+// var config = {
+// 	apiKey: "AIzaSyBadNobcnUDFdZpD_h7AFyn-V5AhLV4M34",
+// 	authDomain: "gottogo-dbe65.firebaseapp.com",
+// 	databaseURL: "https://gottogo-dbe65.firebaseio.com",
+// 	storageBucket: "gottogo-dbe65.appspot.com",
+// };
+  
+// firebase.initializeApp(config);
 
-   		
+// database = firebase.database();
+
+$('#startButton').on('click', function () {
+	destination = $('#destination').val().trim();
+	startDate = $('#startDate').val();  //might need adjustment for moment.js
+	endDate = $('#endDate').val(); //might need adjustment for moment.js
+
+	$('#mapStart').fadeOut(2000);
+	$('#startContainer').fadeOut(2000);
+
    })
+
 });
