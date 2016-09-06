3f81debb922da3ccb34cc7d09c3b13edb8e3396c
Alexander von Bredow <alvb92@gmail.com>
2016-09-06 19:29:05 -0400

    map formatting and ajax functionality

--- assets/javascript/planner.js
+++ assets/javascript/planner.js
@@ -17,31 +17,26 @@ var database = firebase.database();
 var destination = "";
 var startDate = "";
 var endDate = "";
-var days = "";
-var city = "";
+// var days = "";
+// var city = "";
 
 // button clicks
 $('#startButton').on('click', function () {
+
 	destination = $('#pac-input').val().trim();
 	startDate = $('#startDate').val();
 	endDate = $('#endDate').val();
 
-//AJAX call to retrieve coordinates of string value google search	(obj > geometry > location for coords)
-	var city1 = localStorage.getItem('destination')
-	var key1 = 'AIzaSyBQEj4ozak2ZfYSTfrVB8nEhOnHe4dUWSA'
-	var URL1 =  "https://maps.googleapis.com/maps/api/geocode/json?address=" + city1 + "&key=" + key1;
-	// ajax call
-	$.ajax({url: URL1, method: 'GET'}).done(function(response1) {
-				
-		console.log(response1);
-
-	});
-
 	// switch to firebase eventually
-	localStorage.clear();
+	// localStorage.clear();
 	localStorage.setItem("destination", destination); 
 	localStorage.setItem("startDate", startDate);
 	localStorage.setItem("endDate", endDate);
+	//localStorage.setItem("destinationLat", lat1)
+//console.log(lat1)
+
+
+	// debugger;
 })
 
 // make this only run on a certain page?
@@ -50,35 +45,54 @@ $("#destinationDisplay").html(localStorage.getItem("destination"));
 $("#startDateDisplay").html(moment(localStorage.getItem("startDate")).format("M/D/YY"));
 $("#endDateDisplay").html(moment(localStorage.getItem("endDate")).format("M/D/YY"));
 
-// populates main body planner with dynamic days and weather
-var city = "37.8,-122.4";
+// populates main body planner with dynamic days and weather forecast (or historical weather info)
+var coordinates = localStorage.getItem("destinationLat") + "," + localStorage.getItem("destinationLong");
+var days = moment(localStorage.getItem("endDate")).diff(moment(localStorage.getItem("startDate")), "days") + 1;
 var key = "1d150de371b4d1a3"
-var URL = "http://api.wunderground.com/api/" + key + "/forecast10day/geolookup/q/" + city + ".json";
+var weatherForecastURL = "http://api.wunderground.com/api/" + key + "/forecast10day/geolookup/q/" + coordinates + ".json";
 
-// ajax call
-$.ajax({url: URL, method: 'GET'}).done(function(response) {
-			
-	// console.log(response);
-
-	days = moment(localStorage.getItem("endDate")).diff(moment(localStorage.getItem("startDate")), "days") + 1;
-
+if (localStorage.getItem("startDate") > moment().add(10, "days").format("YYYY-MM-DD")) {
 	for (var i = 1; i <= days; i++) {
+		var dateHistorical = moment(localStorage.getItem("startDate")).add([i] - 1, "days").format("MMDD");
+		var weatherPlannerURL = "http://api.wunderground.com/api/" + key + "/planner_" + dateHistorical + dateHistorical + "/q/" + coordinates + ".json";
+
+		$.ajax({url: weatherPlannerURL, method: 'GET'}).done(function(response) {	
+			// console.log(response);
+			high = response.trip.temp_high.avg.F;
+			low = response.trip.temp_low.avg.F;
+		})
+
 		var newDay = $("<div>");
 		var newDayContent = $("<div>");
-		var calendarDay = moment(localStorage.getItem("startDate")).add([i] - 1, "days").format("M/D/YY");
-		var high = response.forecast.simpleforecast.forecastday[i].high.fahrenheit;
-		var low = response.forecast.simpleforecast.forecastday[i].low.fahrenheit;
-		var forecast = response.forecast.simpleforecast.forecastday[i].conditions;
-		var weekday = moment(calendarDay).format("dddd");
+		var calendarDay = moment(localStorage.getItem("startDate")).add([i] - 1, "days").format("dddd - M/D/YY");
 		newDay.addClass("card");
 		newDayContent.addClass("card-content");
-		newDayContent.append("<span class='card-title'><span class='orange-text text-darken-4 dayHeader'>Day " + [i] + "</span><span class='black-text'> " + weekday + " - " + calendarDay + "</span>");
-		newDayContent.append("<p><span id='day" + [i] + "Weather'>High: " + high + "&deg;F | Low: " + low + "&deg;F | Forecast: " + forecast + "</span></p>");
+		newDayContent.append("<span class='card-title'><span class='orange-text text-darken-4 dayHeader'>Day " + [i] + "</span><span class='black-text'> " + calendarDay + "</span>");
+		newDayContent.append("<p><span id='day" + [i] + "Weather'>Avg High: " + "high" + "&deg;F | Avg Low: " + "low" + "&deg;F</span></p>");
 		newDayContent.append("<p>The day's activities</p>");
 		newDay.append(newDayContent);
-
 		$("#planner").append(newDay);
 	}
-});
+} 
+
+else {
+	$.ajax({url: weatherForecastURL, method: 'GET'}).done(function(response) {	
+		for (var i = 1; i <= days; i++) {
+			var newDay = $("<div>");
+			var newDayContent = $("<div>");
+			var calendarDay = moment(localStorage.getItem("startDate")).add([i] - 1, "days").format("dddd - M/D/YY");
+			var high = response.forecast.simpleforecast.forecastday[i].high.fahrenheit;
+			var low = response.forecast.simpleforecast.forecastday[i].low.fahrenheit;
+			var forecast = response.forecast.simpleforecast.forecastday[i].conditions;
+			newDay.addClass("card");
+			newDayContent.addClass("card-content");
+			newDayContent.append("<span class='card-title'><span class='orange-text text-darken-4 dayHeader'>Day " + [i] + "</span><span class='black-text'> " + calendarDay + "</span>");
+			newDayContent.append("<p><span id='day" + [i] + "Weather'>High: " + high + "&deg;F | Low: " + low + "&deg;F | Forecast: " + forecast + "</span></p>");
+			newDayContent.append("<p>The day's activities</p>");
+			newDay.append(newDayContent);
+			$("#planner").append(newDay);
+		}
+	});
+}
 
 // });
