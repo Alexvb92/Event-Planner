95bf593046591bb8dd0554c068fc638a058991dc
Nicolas Caceres <nicolas.caceres89@gmail.com>
2016-09-11 10:00:51 -0400

    code cleanup and about page additions

--- assets/javascript/planner.js
+++ assets/javascript/planner.js
@@ -10,8 +10,8 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
 // button clicks
+// sets destination, start date and end date to local storage
 $('#startButton').on('click', function () {
 
 	destination = $('#pac-input').val().trim();
@@ -26,9 +26,9 @@ $('#startButton').on('click', function () {
 
 // enables datepicker on front page
 $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 5, // Creates a dropdown of 15 years to control year
-    min: true,
+    selectMonths: true, // dropdown to control month
+    selectYears: 5, // dropdown to control year
+    min: true, //limits selection
     format: 'm/d/yy',
 });
 
@@ -37,15 +37,16 @@ $(".destinationDisplay").html(localStorage.getItem("destination"));
 $(".startDateDisplay").html(moment(localStorage.getItem("startDate")).format("M/D/YY"));
 $(".endDateDisplay").html(moment(localStorage.getItem("endDate")).format("M/D/YY"));
 
+// generates map on main.html and runs weatherU function
 var destination = localStorage.getItem('destination')
 var lat1;
 var long1;
 
-    // retrieve coordinates of string value google search (response1.results[0].geometry.location. for coords)
+// retrieve coordinates of string value google search (response1.results[0].geometry.location. for coords)
 var key1 = 'AIzaSyBQEj4ozak2ZfYSTfrVB8nEhOnHe4dUWSA';
 var URL1 =  "https://maps.googleapis.com/maps/api/geocode/json?address=" + destination + "&key=" + key1;
 var google;
-    // ajax call
+
 $.ajax({url: URL1, method: 'GET'}).done(function(response1) { 
 
   	lat1 = Number(response1.results[0].geometry.location.lat);
@@ -54,101 +55,17 @@ $.ajax({url: URL1, method: 'GET'}).done(function(response1) {
   	localStorage.setItem("destinationLong", long1);
 
 	initAutocomplete()
-	weatherb()
+	weatherUnderground()
 });
 
-
-// function initAutocomplete() {
-//     var map = new google.maps.Map(document.getElementById('mapMain'), {
-//       	 center: {lat: lat1, lng: long1},
-//      	 zoom: 14,
-//  	 	 disableDefaultUI: true,
-//     	 mapTypeId: 'roadmap'
-//     });
-
-//     // Create the search box and link it to the UI element.
-//     var input = document.getElementById('pac-input1');
-//     var searchBox = new google.maps.places.SearchBox(input);
-//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-//     // Bias the SearchBox results towards current map's viewport.
-//     map.addListener('bounds_changed', function() {
-//       searchBox.setBounds(map.getBounds());
-//     });
-
-//     var markers = [];
-//     // Listen for the event fired when the user selects a prediction and retrieve
-//     // more details for that place.
-//     searchBox.addListener('places_changed', function() {
-//       var places = searchBox.getPlaces();
-
-//       if (places.length == 0) {
-//         return;
-//       }
-
-//       // Clear out the old markers.
-//       markers.forEach(function(marker) {
-//         marker.setMap(null);
-//       });
-//       markers = [];
-
-//       // For each place, get the icon, name and location.
-//       var bounds = new google.maps.LatLngBounds();
-
-//       places.forEach(function(place) {
-//         if (!place.geometry) {
-//           console.log("Returned place contains no geometry");
-//           return;
-//         }
-//         var icon = {
-//           url: place.icon,
-//           size: new google.maps.Size(71, 71),
-//           origin: new google.maps.Point(0, 0),
-//           anchor: new google.maps.Point(17, 34),
-//           scaledSize: new google.maps.Size(25, 25)
-//         };
-
-//         // Create a marker for each place.
-//         markers.push(new google.maps.Marker({
-//           map: map,
-//           icon: icon,
-//           title: place.name,
-//           position: place.geometry.location
-//         }));
-
-//         if (place.geometry.viewport) {
-//           // Only geocodes have viewport.
-//           bounds.union(place.geometry.viewport);
-//         } else {
-//           bounds.extend(place.geometry.location);
-//         }
-
-//       });
-//       map.fitBounds(bounds);
-//     });
-//   }
-
-//   google.maps.event.addListener(marker, 'click', function() {
-//     service.getDetails(place, function(result, status) {
-//       if (status !== google.maps.places.PlacesServiceStatus.OK) {
-//         console.error(status);
-//         return;
-//       }
-//       infoWindow.setContent(result.name);
-//       infoWindow.open(map, marker);
-//    });
-// });
-
-
-
-function weatherb(){
+function weatherUnderground(){
 	// populates main body planner with dynamic days and weather forecast (or historical weather info)
 	var coordinates = localStorage.getItem("destinationLat") + "," + localStorage.getItem("destinationLong");
 	var days = moment(localStorage.getItem("endDate")).diff(moment(localStorage.getItem("startDate")), "days") + 1;
 	var key = "1d150de371b4d1a3"
 	var weatherForecastURL = "https://api.wunderground.com/api/" + key + "/forecast10day/geolookup/q/" + coordinates + ".json";
 
-	// generates weather summary if forecast is unavailable
+	// generates weather summary if forecast is unavailable (see line below)
 	if (localStorage.getItem("startDate") > moment().add(10, "days").format("M/D/YY")) {
 		var dateHistoricalStart = moment(localStorage.getItem("startDate")).format("MMDD");
 		var dateHistoricalEnd = moment(localStorage.getItem("endDate")).format("MMDD");
@@ -222,6 +139,7 @@ $('#planner').keypress(function(e) {
 var firebaseArray = [];
 var arrayCount = 1;
 
+// displays to do list from firebase
 database.ref().on("child_added", function(childSnapshot) {
 
 	var element = {
@@ -248,51 +166,47 @@ database.ref().on("child_added", function(childSnapshot) {
 	arrayCount++;
 
 	}, function (errorObject) {
-		// In case of error this will print the error
 	  	console.log("The read failed: " + errorObject.code);
 	})
 
+// remove item from html and firebase
 $(document.body).on('click', '.xyz', function () {
-
 	var todoNumber = $(this).attr("toDo");
 	$("#item-" + todoNumber).remove();
-
-	// console.log(firebaseArray[todoNumber]);
-
 	firebaseArray[todoNumber].ref.remove();
-
 });
 
 var checkCounter = 5;
 toDoCount = 0;
 var textInput;
 
-
+// input to do items into html and firebase
 $('#text-input').keypress(function(e) {
-  if(e.which == 13) {
-  	console.log("entered")
-    textInput = $('#text-input').val().trim();
-    newLine = $('<p id="newEntry" class="col s11"><input type="checkbox" id="test' + checkCounter + '" /><label id="textInput" for="test' + checkCounter + '">' + textInput + '</label></p>');
+	if(e.which == 13) {
+		textInput = $('#text-input').val().trim();
+		newLine = $('<p id="newEntry" class="col s11"><input type="checkbox" id="test' + checkCounter + '" /><label id="textInput" for="test' + checkCounter + '">' + textInput + '</label></p>');
 
-	newLine.attr("toDo", toDoCount);
-	newLine.attr("id", "item-" + toDoCount);
-	$('#text-input').val("");
+		newLine.attr("toDo", toDoCount);
+		newLine.attr("id", "item-" + toDoCount);
+		$('#text-input').val("");
 
-	checkCounter++;
-	toDoCount++;
+		checkCounter++;
+		toDoCount++;
 
-	database.ref().push({
-		name: {
-		textInput: textInput,
-		checkCounter: checkCounter,
-		toDo: toDoCount + 1
-		}
-	});
-  }  
+		database.ref().push({
+			name: {
+				textInput: textInput,
+				checkCounter: checkCounter,
+				toDo: toDoCount + 1
+			}
+		});
+	}  
 });
 
-$(".button-collapse").sideNav(); //Enables the slide out menu on mobile
+// enables slide out menu on mobile
+$(".button-collapse").sideNav(); 
 
+// generates map on index.html
 function initAutocomplete1() {
     var map = new google.maps.Map(document.getElementById('mapStart'), {
       
@@ -318,13 +232,11 @@ function initAutocomplete1() {
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
-    $('#pac-input').removeAttr(style) //clears google's position:absolute and top:0 left: 0
+    $('#pac-input').removeAttr(style); //clears google's position:absolute and top:0 left: 0
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
+    	searchBox.setBounds(map.getBounds());
     });
 }
-
-// });
